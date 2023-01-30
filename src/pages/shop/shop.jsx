6d8ce64ab5caf7bb76947519015087435bbd585c
Juancapp <juanitocamo3@gmail.com>
2023-01-29 21:02:23 -0300

    fix filter products bug

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -13,7 +13,7 @@ import "./shop.css";
 export const Shop = () => {
   const { products, getProductsByCategory } = useContext(ShopContext);
   const [productsToDisplay, setProductsToDisplay] = useState([]);
-  const [search, setSearch] = useState();
+  const search = useRef("");
   const category = useRef("all");
   const [orderPrice, setOrderPrice] = useState("default");
 
@@ -31,18 +31,22 @@ export const Shop = () => {
   };
 
   useEffect(() => {
-    orderProducts();
     setProductsToDisplay(orderProducts());
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [orderPrice, orderProducts()]);
+  }, [orderProducts()]);
 
   const handleChange = (e) => {
-    setSearch(e.target.value);
+    search.current = e.target.value;
   };
 
   const onClickSearchBttn = () => {
-    let productsFiltered = productsToDisplay.filter((product) =>
-      product.title.toString().toLowerCase().includes(search.toLowerCase())
+    setProductsToDisplay(products);
+    orderProducts();
+    let productsFiltered = products.filter((product) =>
+      product.title
+        .toString()
+        .toLowerCase()
+        .includes(search.current.toLowerCase())
     );
     setProductsToDisplay(productsFiltered);
   };
@@ -61,37 +65,39 @@ export const Shop = () => {
       <div className="shopTitle">
         <h4>Offers! {`(Work in progress)`}</h4>
       </div>
-      <div className="searchBar">
-        <MagnifyingGlass size={24} />
-        <input
-          type="text"
-          className="modern-input"
-          onChange={handleChange}
-          ref={search}
-          placeholder="Search product..."
-        />
-        <button onClick={onClickSearchBttn}>Search</button>{" "}
+      <div className="inputs">
+        <div className="searchBar">
+          <MagnifyingGlass size={24} />
+          <input
+            type="text"
+            className="modern-input"
+            onChange={handleChange}
+            ref={search}
+            placeholder="Search product..."
+          />
+          <button onClick={onClickSearchBttn}>Search</button>{" "}
+        </div>
+        <select
+          name="selectCategory "
+          id="selectCategory"
+          onChange={(e) => handleSelectCategory(e)}
+        >
+          <option value="all">All</option>
+          <option value={mensClothing}>Men's Clothing</option>
+          <option value={womensClothing}>Women's Clothing</option>
+          <option value={jewelery}>Jewelery</option>
+          <option value={electronics}>Electronics</option>
+        </select>
+        <select
+          name="orderPrice "
+          id="orderPrice"
+          onChange={(e) => handleSelectOrderPrice(e)}
+        >
+          <option value="default">Default</option>
+          <option value="ascending">Ascending</option>
+          <option value="descending">Descending</option>
+        </select>
       </div>
-      <select
-        name="selectCategory "
-        id="selectCategory"
-        onChange={(e) => handleSelectCategory(e)}
-      >
-        <option value="all">All</option>
-        <option value={mensClothing}>Men's Clothing</option>
-        <option value={womensClothing}>Women's Clothing</option>
-        <option value={jewelery}>Jewelery</option>
-        <option value={electronics}>Electronics</option>
-      </select>
-      <select
-        name="orderPrice "
-        id="orderPrice"
-        onChange={(e) => handleSelectOrderPrice(e)}
-      >
-        <option value="default">Default</option>
-        <option value="ascending">Ascending</option>
-        <option value="descending">Descending</option>
-      </select>
       <div className="products">
         {products.length > 0 ? (
           productsToDisplay.length > 0 ? (
