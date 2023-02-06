5416a7558e142abea17c4b013231abaf6997adbc
Juancapp <juanitocamo3@gmail.com>
2023-02-06 20:42:31 -0300

    a

--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -12,7 +12,8 @@ const getProducts = async (
   try {
     setProducts([]);
     setFetched(false);
-    const result = await axios.get(process.env.REACT_APP_API_URL);
+    const result = await axios.get(`${process.env.REACT_APP_API_URL}/`);
+    console.log(result.data.data)
     setProducts(result.data.data);
     if (result.data.data.length > 0) {
       setFetched(true);
